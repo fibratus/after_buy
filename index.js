f1f28bbf2f8cbe6e3c5a5fe537eabe46ed5d5bbc
fibratus <95601342+fibratus@users.noreply.github.com>
2022-11-04 08:41:27 +0900

    basic oauth setup

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
-  }, (accessToken) => {
-    console.log(accessToken)
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile:', profile)
   })
 )
 
@@ -19,6 +21,8 @@ app.get(
     scope: ['profile', 'email']
   })
 )
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
