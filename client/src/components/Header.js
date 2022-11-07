1a6976c8cb806470065e4ca8309f65a12011531e
fibratus <95601342+fibratus@users.noreply.github.com>
2022-11-07 16:06:00 +0900

    feat: basic header design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,14 @@ class Header extends Component {
   render() {
     return (
       <div>
-        Header
+        <div className="nav-wrapper">
+          <a className="left brand-logo">애프터바이</a>
+          <ul className="right">
+            <li>
+              <a>구글로 로그인하기</a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
