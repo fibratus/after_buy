30a435249a93e3a704a8309f911fd32ce5333b37
fibratus <95601342+fibratus@users.noreply.github.com>
2022-11-07 15:52:18 +0900

    feat: basic routing & for prototype  materializecss install

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux'
+import reducers from './reducers'
+
+const store = createStore(reducers, {}, applyMiddleware())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
