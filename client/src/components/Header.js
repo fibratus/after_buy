259ea2cbca38b8f5e82feaa35de8b00debb9a197
fibratus <95601342+fibratus@users.noreply.github.com>
2022-11-08 14:21:51 +0900

    feat: redirect

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 
 class Header extends Component {
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">구글로 로그인하기</a></li>
+        )
+      default:
+        return <li><a>로그아웃</a></li>
+    } 
+  }
+
   render() {
     return (
-      <div>
+      <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">애프터바이</a>
+          <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">애프터바이</Link>
           <ul className="right">
             <li>
-              <a>구글로 로그인하기</a>
+              {this.renderContent()}
             </li>
           </ul>
         </div>
-      </div>
+      </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+  return {auth}
+}
+
+export default connect()(Header);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Landing.module.css'
+
+const Landing = () => {
+  return (
+    <div className={style.landing}>
+      <h1>애프터바이</h1>
+      제품, 서비스 개선을 위한 최적의 플랫폼
+    </div>
+  );
+};
+
+export default Landing;
