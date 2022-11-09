401bd911edb70c6b51f89442fa224e186e938dbe
fibratus <95601342+fibratus@users.noreply.github.com>
2022-11-09 10:44:42 +0900

    survey model

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys')
 require('./models/User')
 require('./services/passport')
+require('./services/Survey')
 
 mongoose.connect(keys.mongoURI);
 
