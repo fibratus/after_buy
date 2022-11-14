529cc760d9088cad24d0eb868738d87055641ac4
fibratus <95601342+fibratus@users.noreply.github.com>
2022-11-14 21:18:28 +0900

    card number how

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,8 +7,12 @@ const Landing = () => {
       <h1 className={styles.landing}>
         애프터바이
       </h1>
-      
+      <div>
       제품, 서비스 개선을 위한 최적의 플랫폼
+      </div>
+      <div>
+      코인 구매할시 카드번호 4242 4242 4242 4242 로 테스트 구매가능.
+      </div>
     </div>
   );
 };
